bf491213365662cddf3e097ce8dcc2bfa53c75ba
loseardes77 <loseardes77@gmail.com>
2024-05-14 19:53:59 +0200

    Add text output

--- src/main.rs
+++ src/main.rs
@@ -26,37 +26,11 @@ const HYPRWAYLAND_SCANNER_REPO: &str = "https://github.com/hyprwm/hyprwayland-sc
 const XDG_DESKTOP_PORTAL_HYPRLAND_REPO: &str = "https://github.com/hyprwm/xdg-desktop-portal-hyprland.git";
 
 fn main() {
-    gtk::init().expect("Failed to initialize GTK.");
     let app = Application::builder()
         .application_id("org.hyprland.hyprupdate")
         .build();
 
-    let (sender, receiver): (Sender<String>, Receiver<String>) = channel();
-
-        sender.send("Hyprupdate started.".to_string()).unwrap();
-        let terminal_output = gtk::TextView::new();
-        terminal_output.set_editable(false);
-        terminal_output.set_cursor_visible(false);
-        terminal_output.set_wrap_mode(gtk::WrapMode::Word);
-        terminal_output.set_vexpand(true);
-        terminal_output.set_hexpand(true);
-
-        let terminal_output1 = terminal_output.clone();
-
-        let scrolled_window = gtk::ScrolledWindow::new(gtk::Adjustment::NONE, gtk::Adjustment::NONE);
-        scrolled_window.set_vexpand(true);
-        scrolled_window.set_hexpand(true);
-        scrolled_window.set_min_content_height(200);
-        scrolled_window.set_min_content_width(800);
-        scrolled_window.set_child(Some(&terminal_output));
-
-        let scrolled_window1 = scrolled_window.clone();
-    
-
-    app.connect_activate(move |app| {
-        let sender1 = sender.clone();
-        let sender2 = sender.clone();
-        let sender3 = sender.clone();
+    app.connect_activate(|app| {
 
         let data_path: String = get_data_path();
 
@@ -64,18 +38,48 @@ fn main() {
             create_dir_all(&data_path).expect("Failed to create directory");
         }
 
+
+
         let meta = Metadata::new(&data_path);
         let meta1 = meta.clone();
         let meta2 = meta.clone();
         let meta3 = meta.clone();
 
-        // We create the main window.
+                // We create the main window.
         let win = ApplicationWindow::builder()
             .application(app)
-            .default_width(320)
-            .default_height(200)
+            .default_width(800)
+            .default_height(600)
             .title("Hyprupdate")
             .build();
+
+
+        thread::sleep(std::time::Duration::from_millis(20));
+        let terminal_output = gtk::TextView::new();
+        terminal_output.set_editable(false);
+        terminal_output.set_cursor_visible(false);
+        terminal_output.set_wrap_mode(gtk::WrapMode::Word);
+        terminal_output.set_vexpand(true);
+        terminal_output.set_hexpand(true);
+    
+        let terminal_output1 = terminal_output.clone();
+        let terminal_output2 = terminal_output.clone();
+        let terminal_output3 = terminal_output.clone();
+    
+        let scrolled_window = gtk::ScrolledWindow::new(gtk::Adjustment::NONE, gtk::Adjustment::NONE);
+        scrolled_window.set_vexpand(true);
+        scrolled_window.set_hexpand(true);
+        scrolled_window.set_min_content_height(400);
+        scrolled_window.set_min_content_width(win.allocated_width() - 20);
+        scrolled_window.set_policy(gtk::PolicyType::Never, gtk::PolicyType::Always);
+        scrolled_window.set_child(Some(&terminal_output));
+    
+        let scrolled_window1 = scrolled_window.clone();
+        let scrolled_window2 = scrolled_window.clone();
+        let scrolled_window3 = scrolled_window.clone();
+        let scrolled_window4 = scrolled_window.clone();
+        
+        update_terminal_output(scrolled_window, terminal_output, "Welcome to Hyprupdate\n".to_string());
 
         let mut software = Vec::new();
 
@@ -121,17 +125,35 @@ fn main() {
             .valign(gtk::Align::Center)
             .build();
         
+        let hypr_software_box1 = gtk::Box::builder()
+            .orientation(gtk::Orientation::Vertical)
+            .spacing(20)
+            .margin_top(20)
+            .margin_bottom(20)
+            .halign(gtk::Align::Center)
+            .valign(gtk::Align::Center)
+            .build();
 
+        let hypr_software_box2 = gtk::Box::builder()
+            .orientation(gtk::Orientation::Vertical)
+            .spacing(20)
+            .margin_top(20)
+            .margin_bottom(20)
+            .halign(gtk::Align::Center)
+            .valign(gtk::Align::Center)
+            .build();
 
-        hypr_software_box.add(&hyprland.clone());
+        hypr_software_box.add(&hyprland);
         hypr_software_box.add(&hyprlock);
         hypr_software_box.add(&hypridle);
-        hypr_software_box.add(&hyprlang);
-        hypr_software_box.add(&hyprcursor);
-        hypr_software_box.add(&hyprpaper);
-        hypr_software_box.add(&hyprpicker);
-        hypr_software_box.add(&hyprwayland_scanner);
-        hypr_software_box.add(&xdg_desktop_portal_hyprland);
+
+        hypr_software_box1.add(&hyprlang);
+        hypr_software_box1.add(&hyprcursor);
+        hypr_software_box1.add(&hyprpaper);
+
+        hypr_software_box2.add(&hyprpicker);
+        hypr_software_box2.add(&hyprwayland_scanner);
+        hypr_software_box2.add(&xdg_desktop_portal_hyprland);
 
         let get_installed_btn = gtk::Button::with_label("Get Installed");
         get_installed_btn.connect_clicked(move |_| {
@@ -165,11 +187,12 @@ fn main() {
                         #[cfg(debug_assertions)]
                         println!("\x1b[34m{} up to date.\x1b[0m", name);
 
-                        sender2.send(format!("{} up to date.", name)).unwrap();
+                        
+                        update_terminal_output(scrolled_window1.clone(), terminal_output1.clone(), format!("{} up to date.", name));
                     } else {
                         #[cfg(debug_assertions)]
                         println!("\x1b[34mUpdated {}\x1b[0m", name);
-                        sender2.send(format!("\x1b[34mUpdated {}\x1b[0m", name)).unwrap();
+                        update_terminal_output(scrolled_window1.clone(), terminal_output1.clone(), format!("Updated {}\n", name));
                         meta.set_to_compile(&name, true);
                     }
                 }
@@ -184,7 +207,8 @@ fn main() {
                     if metadata(get_data_path() + i.label().unwrap().as_str()).is_err() {
                         #[cfg(debug_assertions)]
                         println!("\x1b[32mCloning: {}\x1b[0m", i.label().unwrap().as_str());
-                        sender3.send(format!("\x1b[32mCloning: {}\x1b[0m", i.label().unwrap().as_str())).unwrap();
+
+                        update_terminal_output(scrolled_window3.clone(), terminal_output3.clone(), format!("Cloning: {}\n", i.label().unwrap().as_str()));
                         let repo = Repository::clone(
                             which_repo(&i.label().unwrap().as_str()),
                             data_dir.clone() + i.label().unwrap().as_str(),
@@ -195,7 +219,7 @@ fn main() {
                     }
                     meta1.set_installed(&i.label().unwrap().as_str(), true);
                     meta1.set_to_compile(&i.label().unwrap().as_str(), true);
-                    compile(&i.label().unwrap().as_str(), meta1.clone(), sender3.clone());
+                    compile(&i.label().unwrap().as_str(), meta1.clone(), scrolled_window3.clone(), terminal_output3.clone());
                 }
             }
         });
@@ -204,7 +228,9 @@ fn main() {
         uninstall_selected_btn.connect_clicked(move |_| {
             for i in &software3 {
                 if i.is_active() {
-                    sender1.send(format!("\x1b[31mUninstalling: {}\x1b[0m", i.label().unwrap().as_str())).unwrap();
+                    #[cfg(debug_assertions)]
+                    println!("\x1b[31mUninstalling: {}\x1b[0m", i.label().unwrap().as_str());
+                    update_terminal_output(scrolled_window2.clone(), terminal_output2.clone(), format!("::Uninstalling {}\n", i.label().unwrap().as_str()));
                     remove_dir_all(get_data_path() + i.label().unwrap().as_str())
                         .expect("Failed to remove directory");
                     meta2.set_installed(&i.label().unwrap().as_str(), false);
@@ -244,8 +270,21 @@ fn main() {
 
         win.set_titlebar(Some(&bar));
 
-        box_up.add(&scrolled_window);
-        box_up.add(&hypr_software_box);
+        box_up.add(&scrolled_window4);
+        let hyprbox = gtk::Box::builder()
+            .orientation(gtk::Orientation::Horizontal)
+            .spacing(20)
+            .margin_top(20)
+            .margin_bottom(20)
+            .halign(gtk::Align::Center)
+            .valign(gtk::Align::Center)
+            .build();
+
+        hyprbox.add(&hypr_software_box);
+        hyprbox.add(&hypr_software_box1);
+        hyprbox.add(&hypr_software_box2);
+
+        box_up.add(&hyprbox);
 
         box_down.add(&get_installed_btn);
         box_down.add(&update_btn);
@@ -258,31 +297,24 @@ fn main() {
         win.set_child(Some(&main_box));
 
         win.show_all();
+        
+        scrolled_window4.set_width_request(win.allocated_width() - 20);
+        win.connect_resize_mode_notify(move |win| {
+            scrolled_window4.set_width_request(win.allocated_width() - 20);
+        });
+
     });
 
-    let scrolled_window1 = Arc::new(Mutex::new(scrolled_window1.clone()));
-    let cloned_scrolled_window1 = Arc::clone(&scrolled_window1);
-    let terminal_output1 = Arc::new(Mutex::new(terminal_output1.clone()));
-    let cloned_terminal_output1 = Arc::clone(&terminal_output1);
-    thread::spawn(|| {
-        update_terminal_output(receiver, cloned_scrolled_window1, cloned_terminal_output1);
-    });
+    app.run();
 
 }
 
-fn update_terminal_output(receiver: Receiver<String>, scrolled_window: Arc<Mutex<gtk::ScrolledWindow>>, terminal_output: Arc<Mutex<gtk::TextView>>) {
-    loop {
-        match receiver.try_recv() {
-            Ok(text) => {
-                let buffer = terminal_output.lock().unwrap().buffer().unwrap();
-                let mut end_iter = buffer.end_iter();
-                buffer.insert(&mut end_iter, text.as_str());
-
-                scrolled_window.lock().unwrap().vadjustment().set_value(scrolled_window.lock().unwrap().vadjustment().upper());
-            }
-            Err(_) => {},
-        }
-    }
+fn update_terminal_output(scrolled_window: gtk::ScrolledWindow, terminal_output: gtk::TextView, message: String) {
+    let buff = terminal_output.buffer().unwrap();
+    buff.insert_at_cursor(&message);
+    let vadj = scrolled_window.vadjustment();
+    vadj.set_value(vadj.upper());
+    scrolled_window.set_vadjustment(Some(&vadj));
 }
 
 fn get_data_path() -> String {
@@ -301,26 +333,26 @@ fn get_data_path() -> String {
     }
 }
 
-fn compile(software: &str, meta: Metadata, sender: Sender<String>) {
+fn compile(software: &str, meta: Metadata, scrolled_window: gtk::ScrolledWindow, terminal_output: gtk::TextView) {
     if !meta.get_to_compile(software) {
         return;
     }
     #[cfg(debug_assertions)]
     println!("\x1b[32mCompiling {}\x1b[0m", software);
-    
-    sender.send(format!("\x1b[32mCompiling {}\x1b[0m", software)).unwrap();
+
+    update_terminal_output(scrolled_window.clone(), terminal_output.clone(), format!("::Compiling {}\n", software));
 
     let path = get_data_path() + software;
     set_current_dir(path.as_str()).expect("Failed to set current directory");
     let build_cmd = meta.get_compile_cmd(software);
-    run_command(build_cmd.as_str(), sender.clone());
+    run_command(build_cmd.as_str(), scrolled_window.clone(), terminal_output.clone());
     meta.set_to_compile(software, false);
     let install_cmd = meta.get_install_cmd(software);
-    run_command(install_cmd.as_str(), sender.clone());
+    run_command(install_cmd.as_str(), scrolled_window.clone(), terminal_output.clone());
     meta.set_installed(software, true);
     #[cfg(debug_assertions)]
     println!("{} installed", software);
-    sender.send(format!("\x1b[32m{} installed\x1b[0m", software)).unwrap();
+    update_terminal_output(scrolled_window.clone(), terminal_output.clone(), format!("::Installed {}\n", software));
 }
 
 fn which_repo(name: &str) -> &str {
@@ -338,12 +370,14 @@ fn which_repo(name: &str) -> &str {
     }
 }
 
-fn run_command(inpt: &str, sender: Sender<String>) {
+fn run_command(inpt: &str, scroll_window: gtk::ScrolledWindow, terminal_output: gtk::TextView) {
     let commands = inpt.trim().split("&&").map(|i| i.trim());
     for command in commands {
         #[cfg(debug_assertions)]
         println!("\x1b[34mRunning: {}\x1b[0m", command);
-        sender.send(format!("\x1b[34mRunning: {}\x1b[0m", command)).unwrap();
+
+        update_terminal_output(scroll_window.clone(), terminal_output.clone(), format!("::Running {}\n", command));
+
         let iterator = command.split_whitespace();
         let mut command = Command::new(iterator.clone().next().unwrap());
         for arg in iterator.skip(1) {
@@ -351,21 +385,23 @@ fn run_command(inpt: &str, sender: Sender<String>) {
         }
         let mut process = command
             .stderr(Stdio::piped())
+            .stdout(Stdio::piped())
             .spawn()
             .expect("failed to build repository");
-
-        let reader = BufReader::new(process.stderr.take().expect("failed to get stdout"));
+        
+        let reader = BufReader::new(process.stdout.take().expect("failed to get stdout"));
         for line in reader.lines() {
             match line {
                 Ok(line) => {
                     #[cfg(debug_assertions)]
                     println!("{}", line);
-                    sender.send(line).unwrap();
+                    update_terminal_output(scroll_window.clone(), terminal_output.clone(), format!("{}\n", line.trim()));
                 },
                 Err(e) => {
                     #[cfg(debug_assertions)]
                     println!("\x1b[31mError: {}\x1b[0m", e);
-                    sender.send(format!("\x1b[31mError: {}\x1b[0m", e)).unwrap();
+                    
+                    update_terminal_output(scroll_window.clone(), terminal_output.clone(), format!("Error: {}\n", e));
                 },
             }
         }
